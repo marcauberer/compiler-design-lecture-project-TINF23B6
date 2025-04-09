146eb2f588b02ccf4e934544a25ac9699bb6170b
Larissa Knopf <larry.knopf@web.de>
2025-04-09 12:39:43 +0200

    create classes

---
+++ src/main/java/com/auberer/compilerdesignlectureproject/lexer/statemachine/DoubleLiteralStateMachine.java
@@ -0,0 +1,15 @@
+package com.auberer.compilerdesignlectureproject.lexer.statemachine;
+
+import com.auberer.compilerdesignlectureproject.lexer.TokenType;
+
+public class DoubleLiteralStateMachine extends StateMachine{
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public TokenType getTokenType() {
+        return null;
+    }
+}
