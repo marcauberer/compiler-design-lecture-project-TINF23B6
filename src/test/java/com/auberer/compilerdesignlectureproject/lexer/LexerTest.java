992a419207bd72c1ed10d4e7ed74b7c13ab32b28
Larissa Knopf <larry.knopf@web.de>
2025-04-18 19:53:13 +0200

    Add Tests but they do not work

--- src/test/java/com/auberer/compilerdesignlectureproject/lexer/LexerTest.java
+++ src/test/java/com/auberer/compilerdesignlectureproject/lexer/LexerTest.java
@@ -26,8 +26,8 @@ public class LexerTest {
         Reader reader = new Reader(input+"");
         assert !reader.isEOF();
         Lexer lexer = new Lexer(reader, false);
-        assert !lexer.isEOF();
         assertDoesNotThrow(() -> lexer.expect(TokenType.TOK_INTEGER_LIT));
+        assert lexer.isEOF();
     }
     @Test
     public void test_DoubleLiteralStateMachine_is_correct_initialised_and_returns_TOK_DOUBLE_LIT(){
@@ -35,8 +35,8 @@ public class LexerTest {
         Reader reader = new Reader(input+"");
         assert !reader.isEOF();
         Lexer lexer = new Lexer(reader, false);
-        assert !lexer.isEOF();
         assertDoesNotThrow(() -> lexer.expect(TokenType.TOK_DOUBLE_LIT));
+        assert lexer.isEOF();
     }
     @Test
     public void test_IdentifierLiteralStateMachine_is_correct_initialised_and_returns_TOK_IDENTIFIER_LIT(){
@@ -44,8 +44,8 @@ public class LexerTest {
         Reader reader = new Reader(input);
         assert !reader.isEOF();
         Lexer lexer = new Lexer(reader, false);
-        assert !lexer.isEOF();
         assertDoesNotThrow(() -> lexer.expect(TokenType.TOK_IDENTIFIER_LIT));
+        assert lexer.isEOF();
     }
     @Test
     public void test_KeyLiteralStateMachine_is_correct_initialised_and_returns_TOK_KEYWORD_LIT(){
@@ -53,7 +53,7 @@ public class LexerTest {
         Reader reader = new Reader(input);
         assert !reader.isEOF();
         Lexer lexer = new Lexer(reader, false);
-        assert !lexer.isEOF();
         assertDoesNotThrow(() -> lexer.expect(TokenType.TOK_KEYWORD_LIT));
+        assert lexer.isEOF();
     }
 }
