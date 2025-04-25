b1ab1db628cdece438655ddbd56696597484155d
aghyy <79050516+aghyy@users.noreply.github.com>
2025-04-25 16:54:42 +0200

    add changes

--- src/main/java/com/auberer/compilerdesignlectureproject/ast/ASTStmtLstNode.java
+++ src/main/java/com/auberer/compilerdesignlectureproject/ast/ASTStmtLstNode.java
@@ -1,21 +1,8 @@
 package com.auberer.compilerdesignlectureproject.ast;
 
-import com.auberer.compilerdesignlectureproject.lexer.TokenType;
-
-import java.util.HashSet;
-import java.util.Set;
-
 public class ASTStmtLstNode extends ASTNode {
   @Override
   public <T> T accept(ASTVisitor<T> visitor) {
     return visitor.visistStmtLst(this);
-  }
-
-  public static Set<TokenType> getSelectionSet() {
-    Set<TokenType> selectionSet = new HashSet<>();
-    selectionSet.add(TokenType.TOK_SWITCH);
-    selectionSet.add(TokenType.TOK_CASE);
-    selectionSet.add(TokenType.TOK_DEFAULT);
-    return selectionSet;
   }
 }
