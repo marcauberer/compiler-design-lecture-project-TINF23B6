d7c66d9a3ab6e31ce63df30a03025b6d6904bf3f
felix <felix.s.haag@web.de>
2025-04-28 13:49:07 +0200

    add switch case

--- src/main/java/com/auberer/compilerdesignlectureproject/parser/Parser.java
+++ src/main/java/com/auberer/compilerdesignlectureproject/parser/Parser.java
@@ -402,5 +402,49 @@ public class Parser implements IParser {
     return node;
   }
 
+  public ASTSwitchCaseStmtNode parseSwitchCaseStmt() {
+    ASTSwitchCaseStmtNode node = new ASTSwitchCaseStmtNode();
+    enterNode(node);
+    
+    lexer.expect(TokenType.TOK_SWITCH);
+    lexer.expect(TokenType.TOK_LPAREN);
+    ASTTernaryExprNode switchExpr = new ASTTernaryExprNode();
+    node.addChild(switchExpr);
+    lexer.expect(TokenType.TOK_RPAREN);
+    lexer.expect(TokenType.TOK_LBRACE);
+
+    do {
+      enterNode(parseCaseStmt(node));
+    } while (lexer.getToken().getType() == TokenType.TOK_CASE);
+
+    if (lexer.getToken().getType() == TokenType.TOK_DEFAULT) {
+      enterNode(parseDefaultStmt(node));
+    }
+
+    lexer.expect(TokenType.TOK_RBRACE);
+
+    exitNode(node);
+    return node;
+  }
+
+  public ASTSwitchCaseStmtNode parseCaseStmt(ASTSwitchCaseStmtNode node) {
+    lexer.expect(TokenType.TOK_CASE);
+    ASTLiteralNode caseLiteral = parseLiteral();
+    node.addChild(caseLiteral);
+    lexer.expect(TokenType.TOK_COLON);
+    ASTStmtLstNode caseStmtLst = parseStmtLst();
+    node.addChild(caseStmtLst);
+
+    return node;
+  }
+
+  public ASTSwitchCaseStmtNode parseDefaultStmt(ASTSwitchCaseStmtNode node) {
+    lexer.expect(TokenType.TOK_DEFAULT);
+    lexer.expect(TokenType.TOK_COLON);
+    ASTStmtLstNode defaultStmtLst = parseStmtLst();
+    node.addChild(defaultStmtLst);
+
+    return node;
+  }
 
 }
