2325f1bef66e2f65a09ef87c49b1495dff5452b3
Justus <vetter.justus.a23@dh-karlsruhe.de>
2025-05-07 11:46:54 +0200

    chore: ASTParamLSTNode
    rename fct

--- src/main/java/com/auberer/compilerdesignlectureproject/ast/ASTParamLstNode.java
+++ src/main/java/com/auberer/compilerdesignlectureproject/ast/ASTParamLstNode.java
@@ -15,5 +15,5 @@ public class ASTParamLstNode extends ASTNode {
     return ASTTypeNode.getSelectionSet();
   }
 
-  public ArrayList<ASTParamNode> getParams() {return getChildren(ASTParamNode.class);}
+  public ArrayList<ASTParamNode> getParam() {return getChildren(ASTParamNode.class);}
 }
