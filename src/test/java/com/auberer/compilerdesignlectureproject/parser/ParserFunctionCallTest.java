b064474ab11d951451a1441b396a1a49daac39c3
Justus <vetter.justus.a23@dh-karlsruhe.de>
2025-05-09 23:33:31 +0200

    feat: update Antler parser

---
+++ src/test/java/com/auberer/compilerdesignlectureproject/parser/ParserFunctionCallTest.java
@@ -0,0 +1,12 @@
+package com.auberer.compilerdesignlectureproject.parser;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class ParserFunctionCallTest {
+    @Test
+    @DisplayName("FullParserTest")
+    public void fullParserTest() {
+
+    }
+}
---
+++ src/test/java/com/auberer/compilerdesignlectureproject/parser/ParserFunctionDefTest.java
@@ -0,0 +1,4 @@
+package com.auberer.compilerdesignlectureproject.parser;
+
+public class ParserFunctionDefTest {
+}
