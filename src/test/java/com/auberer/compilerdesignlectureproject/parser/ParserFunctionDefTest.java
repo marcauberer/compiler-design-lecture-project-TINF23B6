6192dc8208f0fb6ed99c1b3929b4cb75af2f4077
Justus <vetter.justus.a23@dh-karlsruhe.de>
2025-05-12 10:56:08 +0200

    test: add func def test
    add simple function definition tests

--- src/test/java/com/auberer/compilerdesignlectureproject/parser/ParserFunctionDefTest.java
+++ src/test/java/com/auberer/compilerdesignlectureproject/parser/ParserFunctionDefTest.java
@@ -1,5 +1,8 @@
 package com.auberer.compilerdesignlectureproject.parser;
 
+import com.auberer.compilerdesignlectureproject.lexer.Lexer;
+import com.auberer.compilerdesignlectureproject.reader.Reader;
+import com.auberer.compilerdesignlectureproject.reader.ReaderTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
