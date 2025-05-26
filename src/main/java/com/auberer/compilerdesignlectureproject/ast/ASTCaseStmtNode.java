efb52c809691626136c4841ffeb67c4ee93c51a0
aghyy <golea.andreas@yahoo.com>
2025-05-26 18:27:48 +0200

    fixes

--- src/main/java/com/auberer/compilerdesignlectureproject/ast/ASTCaseStmtNode.java
+++ src/main/java/com/auberer/compilerdesignlectureproject/ast/ASTCaseStmtNode.java
@@ -4,8 +4,18 @@ import java.util.HashSet;
 import java.util.Set;
 
 import com.auberer.compilerdesignlectureproject.lexer.TokenType;
+import com.auberer.compilerdesignlectureproject.sema.ExprResult;
+import com.auberer.compilerdesignlectureproject.sema.Scope;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class ASTCaseStmtNode extends ASTNode {
+
+  private ExprResult conditionResult;
+  private Scope scope;
 
   @Override
   public <T> T accept(ASTVisitor<T> visitor) {
--- src/main/java/com/auberer/compilerdesignlectureproject/sema/TypeChecker.java
+++ src/main/java/com/auberer/compilerdesignlectureproject/sema/TypeChecker.java
@@ -27,241 +27,249 @@ import com.auberer.compilerdesignlectureproject.ast.ASTVarDeclNode;
  */
 public class TypeChecker extends ASTSemaVisitor<ExprResult> {
 
-    final ASTEntryNode entryNode;
+  final ASTEntryNode entryNode;
 
-    public TypeChecker(ASTEntryNode entryNode) {
-        this.entryNode = entryNode;
+  public TypeChecker(ASTEntryNode entryNode) {
+    this.entryNode = entryNode;
+  }
+
+  public TypeChecker() {
+    this.entryNode = null;
+  }
+
+  @Override
+  public ExprResult visitEntry(ASTEntryNode node) {
+    assert currentScope.isEmpty();
+    currentScope.push(node.getRootScope());
+
+    visitChildren(node);
+
+    assert currentScope.peek() == node.getRootScope();
+    currentScope.pop();
+    assert currentScope.isEmpty();
+
+    return null;
+  }
+
+  @Override
+  public ExprResult visitPrintBuiltin(ASTPrintBuiltinCallNode node) {
+    ASTTernaryExprNode expr = node.getExpr();
+    visit(expr);
+
+    if (!expr.getType().isOneOf(SuperType.TYPE_INT, SuperType.TYPE_DOUBLE, SuperType.TYPE_STRING)) {
+      throw new SemaError(node, "print() expects an integer, double or string as argument");
     }
 
-    public TypeChecker() {
-        this.entryNode = null;
+    return new ExprResult(new Type(SuperType.TYPE_INVALID));
+  }
+
+  @Override
+  public ExprResult visitLiteral(ASTLiteralNode node) {
+    SuperType resultSuperType = switch (node.getLiteralType()) {
+      case INT -> SuperType.TYPE_INT;
+      case DOUBLE -> SuperType.TYPE_DOUBLE;
+      case STRING -> SuperType.TYPE_STRING;
+      case BOOL -> SuperType.TYPE_BOOL;
+    };
+    Type resultType = new Type(resultSuperType);
+    return new ExprResult(node.setEvaluatedSymbolType(resultType));
+  }
+
+  @Override
+  public ExprResult visitType(ASTTypeNode node) {
+    SuperType resultSuperType = switch (node.getDataType()) {
+      case INT -> SuperType.TYPE_INT;
+      case DOUBLE -> SuperType.TYPE_DOUBLE;
+      case STRING -> SuperType.TYPE_STRING;
+      case BOOL -> SuperType.TYPE_BOOL;
+    };
+    Type resultType = new Type(resultSuperType);
+    return new ExprResult(node.setEvaluatedSymbolType(resultType));
+  }
+
+  @Override
+  public ExprResult visitTernaryExpr(ASTTernaryExprNode node) {
+    if (node.getTrueBranch() != null) {
+      assert node.getFalseBranch() != null;
+
+      visitChildren(node);
+      var condition = node.getCondition();
+      var trueBranch = node.getTrueBranch();
+      var falseBranch = node.getFalseBranch();
+      if (!condition.getType().is(SuperType.TYPE_BOOL))
+        throw new SemaError(node, "Ternary condition must be of type bool");
+
+      if (!trueBranch.getType().is(falseBranch.getType().getSuperType()))
+        throw new SemaError(node, "Ternary branches must have the same type");
+
+      return new ExprResult(node.setEvaluatedSymbolType(trueBranch.getType()));
+    } else {
+      ExprResult result = visit(node.getEqualityExpr());
+      node.setEvaluatedSymbolType(result.getType());
+      return result;
+    }
+  }
+
+  // Team 6
+  @Override
+  public ExprResult visitSwitchCaseStmt(ASTSwitchCaseStmtNode node) {
+    ExprResult conditionResult = visit(node.getCondition());
+    List<ASTCaseStmtNode> caseBlocks = node.getCaseBlocks();
+
+    for (ASTCaseStmtNode caseBlock : caseBlocks) {
+      caseBlock.setConditionResult(conditionResult);
+      visitCaseStmt(caseBlock);
     }
 
-    @Override
-    public ExprResult visitEntry(ASTEntryNode node) {
-        assert currentScope.isEmpty();
-        currentScope.push(node.getRootScope());
-
-        visitChildren(node);
-
-        assert currentScope.peek() == node.getRootScope();
-        currentScope.pop();
-        assert currentScope.isEmpty();
-
-        return null;
+    ASTDefaultStmtNode defaultBlock = node.getDefaultBlock();
+    if (defaultBlock != null) {
+      visit(defaultBlock);
     }
 
-    @Override
-    public ExprResult visitPrintBuiltin(ASTPrintBuiltinCallNode node) {
-        ASTTernaryExprNode expr = node.getExpr();
-        visit(expr);
+    Type resultType = new Type(SuperType.TYPE_INVALID);
+    return new ExprResult(node.setEvaluatedSymbolType(resultType));
+  }
 
-        if (!expr.getType().isOneOf(SuperType.TYPE_INT, SuperType.TYPE_DOUBLE, SuperType.TYPE_STRING)) {
-            throw new SemaError(node, "print() expects an integer, double or string as argument");
-        }
+  @Override
+  public ExprResult visitCaseStmt(ASTCaseStmtNode caseBlock) {
+    Scope scope = caseBlock.getScope();
+    currentScope.push(scope);
 
-        return new ExprResult(new Type(SuperType.TYPE_INVALID));
+    ASTLiteralNode literal = caseBlock.getLiteral();
+    if (literal != null) {
+      ExprResult caseResult = visit(literal);
+      if (!caseResult.getType().is(caseBlock.getConditionResult().getType().getSuperType())) {
+        throw new SemaError(literal, "Case value must be of type " + caseBlock.getConditionResult().getType().getSuperType());
+      }
     }
 
-    @Override
-    public ExprResult visitLiteral(ASTLiteralNode node) {
-        SuperType resultSuperType = switch (node.getLiteralType()) {
-            case INT -> SuperType.TYPE_INT;
-            case DOUBLE -> SuperType.TYPE_DOUBLE;
-            case STRING -> SuperType.TYPE_STRING;
-            case BOOL -> SuperType.TYPE_BOOL;
-        };
-        Type resultType = new Type(resultSuperType);
-        return new ExprResult(node.setEvaluatedSymbolType(resultType));
+    visit(caseBlock.getStmtLst());
+
+    assert currentScope.peek() == scope;
+    currentScope.pop();
+
+    Type resultType = new Type(SuperType.TYPE_INVALID);
+    return new ExprResult(caseBlock.setEvaluatedSymbolType(resultType));
+  }
+
+  @Override
+  public ExprResult visitEqualityExpr(ASTEqualityExprNode node) {
+    var operands = node.getOperands();
+    if (operands.size() == 1) {
+      ExprResult result = visit(operands.getFirst());
+      node.setEvaluatedSymbolType(result.getType());
+      return result;
+    } else {
+      assert operands.size() == 2;
+      visitChildren(node);
+      var left = operands.get(0);
+      var right = operands.get(1);
+      if (!left.getType().is(right.getType().getSuperType()))
+        throw new SemaError(node, "Equality expressions must have the same type");
+      return new ExprResult(node.setEvaluatedSymbolType(new Type(SuperType.TYPE_BOOL)));
+    }
+  }
+
+  @Override
+  public ExprResult visitAdditiveExpr(ASTAdditiveExprNode node) {
+    var operands = node.getOperands();
+    if (operands.size() == 1) {
+      ExprResult result = visit(operands.getFirst());
+      node.setEvaluatedSymbolType(result.getType());
+      return result;
+    } else {
+      assert operands.size() == 2;
+      visitChildren(node);
+      var left = operands.get(0);
+      var right = operands.get(1);
+      if (!left.getType().is(right.getType().getSuperType()))
+        throw new SemaError(node, "Additive expressions must have the same type");
+      return new ExprResult(node.setEvaluatedSymbolType(left.getType()));
+    }
+  }
+
+  @Override
+  public ExprResult visitMultiplicativeExpr(ASTMultiplicativeExprNode node) {
+    var operands = node.getOperands();
+    if (operands.size() == 1) {
+      ExprResult result = visit(operands.getFirst());
+      node.setEvaluatedSymbolType(result.getType());
+      return result;
+    } else {
+      assert operands.size() == 2;
+      visitChildren(node);
+      var left = operands.get(0);
+      var right = operands.get(1);
+      if (!left.getType().is(right.getType().getSuperType()))
+        throw new SemaError(node, "Additive expressions must have the same type");
+      return new ExprResult(node.setEvaluatedSymbolType(left.getType()));
+    }
+  }
+
+  @Override
+  public ExprResult visitAtomicExpr(ASTAtomicExprNode node) {
+    ASTLiteralNode literal = node.getLiteral();
+    if (literal != null) {
+      ExprResult result = visit(literal);
+      node.setEvaluatedSymbolType(result.getType());
+      return result;
     }
 
-    @Override
-    public ExprResult visitType(ASTTypeNode node) {
-        SuperType resultSuperType = switch (node.getDataType()) {
-            case INT -> SuperType.TYPE_INT;
-            case DOUBLE -> SuperType.TYPE_DOUBLE;
-            case STRING -> SuperType.TYPE_STRING;
-            case BOOL -> SuperType.TYPE_BOOL;
-        };
-        Type resultType = new Type(resultSuperType);
-        return new ExprResult(node.setEvaluatedSymbolType(resultType));
+    ASTFunctionCallNode functionCall = node.getFunctionCall();
+    if (functionCall != null) {
+      ExprResult result = visit(functionCall);
+      node.setEvaluatedSymbolType(result.getType());
+      return result;
     }
 
-    @Override
-    public ExprResult visitTernaryExpr(ASTTernaryExprNode node) {
-        if (node.getTrueBranch() != null) {
-            assert node.getFalseBranch() != null;
-
-            visitChildren(node);
-            var condition = node.getCondition();
-            var trueBranch = node.getTrueBranch();
-            var falseBranch = node.getFalseBranch();
-            if (!condition.getType().is(SuperType.TYPE_BOOL))
-                throw new SemaError(node, "Ternary condition must be of type bool");
-
-            if (!trueBranch.getType().is(falseBranch.getType().getSuperType()))
-                throw new SemaError(node, "Ternary branches must have the same type");
-
-            return new ExprResult(node.setEvaluatedSymbolType(trueBranch.getType()));
-        } else {
-            ExprResult result = visit(node.getEqualityExpr());
-            node.setEvaluatedSymbolType(result.getType());
-            return result;
-        }
+    ASTPrintBuiltinCallNode printBuiltinCall = node.getPrintBuiltin();
+    if (printBuiltinCall != null) {
+      ExprResult result = visit(printBuiltinCall);
+      node.setEvaluatedSymbolType(result.getType());
+      return result;
     }
 
-    // Team 6
-    @Override
-    public ExprResult visitSwitchCaseStmt(ASTSwitchCaseStmtNode node) {
-        // Visit the condition to evaluate its type
-        ExprResult conditionResult = visit(node.getCondition());
-        List<ASTCaseStmtNode> caseBlocks = node.getCaseBlocks();
-
-        // Visit all case blocks to evaluate their types
-        for (ASTCaseStmtNode caseBlock : caseBlocks) {
-            // Visit the case literal to get its type
-            ASTLiteralNode literal = caseBlock.getLiteral();
-            if (literal != null) {
-                ExprResult caseResult = visit(literal);
-                if (!caseResult.getType().is(conditionResult.getType().getSuperType())) {
-                    throw new SemaError(literal, "Case value must be of type " + conditionResult.getType().getSuperType());
-                }
-            }
-
-            // Visit the case block's statements
-            visit(caseBlock.getStmtLst());
-        }
-
-        // Visit default block if it exists
-        ASTDefaultStmtNode defaultBlock = node.getDefaultBlock();
-        if (defaultBlock != null) {
-            visit(defaultBlock);
-        }
-
-        // The switch statement itself evaluates to void (TYPE_INVALID)
-        Type resultType = new Type(SuperType.TYPE_INVALID);
-        return new ExprResult(node.setEvaluatedSymbolType(resultType));
+    ASTTernaryExprNode ternaryExpr = node.getTernaryExpr();
+    if (ternaryExpr != null) {
+      ExprResult result = visit(ternaryExpr);
+      node.setEvaluatedSymbolType(result.getType());
+      return result;
     }
 
-    @Override
-    public ExprResult visitEqualityExpr(ASTEqualityExprNode node) {
-        var operands = node.getOperands();
-        if (operands.size() == 1) {
-            ExprResult result = visit(operands.getFirst());
-            node.setEvaluatedSymbolType(result.getType());
-            return result;
-        } else {
-            assert operands.size() == 2;
-            visitChildren(node);
-            var left = operands.get(0);
-            var right = operands.get(1);
-            if (!left.getType().is(right.getType().getSuperType()))
-                throw new SemaError(node, "Equality expressions must have the same type");
-            return new ExprResult(node.setEvaluatedSymbolType(new Type(SuperType.TYPE_BOOL)));
-        }
+    SymbolTableEntry entry = node.getCurrentSymbol();
+    return new ExprResult(node.setEvaluatedSymbolType(entry.getType()));
+  }
+
+  @Override
+  public ExprResult visitVarDecl(ASTVarDeclNode node) {
+    ExprResult lhs = visit(node.getDataType());
+    ExprResult rhs = visit(node.getInitExpr());
+
+    if (!lhs.getType().is(rhs.getType().getSuperType()))
+      throw new SemaError(node, "Type mismatch in variable declaration");
+
+    SymbolTableEntry entry = node.getCurrentSymbol();
+    assert entry != null;
+    entry.setType(lhs.getType());
+
+    return new ExprResult(node.setEvaluatedSymbolType(lhs.getType()));
+  }
+
+  @Override
+  public ExprResult visitAssignExpr(ASTAssignExprNode node) {
+    Type resultType = new Type(SuperType.TYPE_INVALID);
+
+    if (node.isAssignment()) {
+      SymbolTableEntry entry = node.getCurrentSymbol();
+      assert entry != null;
+
+      ExprResult rhs = visit(node.getRhs());
+      if (!entry.getType().is(rhs.getType().getSuperType()))
+        throw new SemaError(node, "Type mismatch in assignment");
+
+      resultType = rhs.getType();
     }
 
-    @Override
-    public ExprResult visitAdditiveExpr(ASTAdditiveExprNode node) {
-        var operands = node.getOperands();
-        if (operands.size() == 1) {
-            ExprResult result = visit(operands.getFirst());
-            node.setEvaluatedSymbolType(result.getType());
-            return result;
-        } else {
-            assert operands.size() == 2;
-            visitChildren(node);
-            var left = operands.get(0);
-            var right = operands.get(1);
-            if (!left.getType().is(right.getType().getSuperType()))
-                throw new SemaError(node, "Additive expressions must have the same type");
-            return new ExprResult(node.setEvaluatedSymbolType(left.getType()));
-        }
-    }
-
-    @Override
-    public ExprResult visitMultiplicativeExpr(ASTMultiplicativeExprNode node) {
-        var operands = node.getOperands();
-        if (operands.size() == 1) {
-            ExprResult result = visit(operands.getFirst());
-            node.setEvaluatedSymbolType(result.getType());
-            return result;
-        } else {
-            assert operands.size() == 2;
-            visitChildren(node);
-            var left = operands.get(0);
-            var right = operands.get(1);
-            if (!left.getType().is(right.getType().getSuperType()))
-                throw new SemaError(node, "Additive expressions must have the same type");
-            return new ExprResult(node.setEvaluatedSymbolType(left.getType()));
-        }
-    }
-
-    @Override
-    public ExprResult visitAtomicExpr(ASTAtomicExprNode node) {
-        ASTLiteralNode literal = node.getLiteral();
-        if (literal != null) {
-            ExprResult result = visit(literal);
-            node.setEvaluatedSymbolType(result.getType());
-            return result;
-        }
-
-        ASTFunctionCallNode functionCall = node.getFunctionCall();
-        if (functionCall != null) {
-            ExprResult result = visit(functionCall);
-            node.setEvaluatedSymbolType(result.getType());
-            return result;
-        }
-
-        ASTPrintBuiltinCallNode printBuiltinCall = node.getPrintBuiltin();
-        if (printBuiltinCall != null) {
-            ExprResult result = visit(printBuiltinCall);
-            node.setEvaluatedSymbolType(result.getType());
-            return result;
-        }
-
-        ASTTernaryExprNode ternaryExpr = node.getTernaryExpr();
-        if (ternaryExpr != null) {
-            ExprResult result = visit(ternaryExpr);
-            node.setEvaluatedSymbolType(result.getType());
-            return result;
-        }
-
-        SymbolTableEntry entry = node.getCurrentSymbol();
-        return new ExprResult(node.setEvaluatedSymbolType(entry.getType()));
-    }
-
-    @Override
-    public ExprResult visitVarDecl(ASTVarDeclNode node) {
-        ExprResult lhs = visit(node.getDataType());
-        ExprResult rhs = visit(node.getInitExpr());
-
-        if (!lhs.getType().is(rhs.getType().getSuperType()))
-            throw new SemaError(node, "Type mismatch in variable declaration");
-
-        SymbolTableEntry entry = node.getCurrentSymbol();
-        assert entry != null;
-        entry.setType(lhs.getType());
-
-        return new ExprResult(node.setEvaluatedSymbolType(lhs.getType()));
-    }
-
-    @Override
-    public ExprResult visitAssignExpr(ASTAssignExprNode node) {
-        Type resultType = new Type(SuperType.TYPE_INVALID);
-
-        if (node.isAssignment()) {
-            SymbolTableEntry entry = node.getCurrentSymbol();
-            assert entry != null;
-
-            ExprResult rhs = visit(node.getRhs());
-            if (!entry.getType().is(rhs.getType().getSuperType()))
-                throw new SemaError(node, "Type mismatch in assignment");
-
-            resultType = rhs.getType();
-        }
-
-        return new ExprResult(node.setEvaluatedSymbolType(resultType));
-    }
+    return new ExprResult(node.setEvaluatedSymbolType(resultType));
+  }
 }
-
